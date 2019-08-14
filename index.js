4acaf9b8b3bbb814c4e51ffd9d7e90fd677f6bce
HuiloIvanovich <alex4152001@mail.ru>
2019-08-14 20:48:52 +0300

    Changes for heroku

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 require('./app/bot.js')
+const http = require("http");
+const PORT = process.env.port;
+
+http.createServer((req, res) => {
+    console.log("response from server");
+}).listen(PORT);
 
