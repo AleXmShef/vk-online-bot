d4a1c8aef279b5cd76d7959a3095010b55d0c505
HuiloIvanovich <alex4152001@mail.ru>
2020-02-02 05:00:31 +0300

    123

--- app/bot.js
+++ app/bot.js
@@ -149,11 +149,11 @@ const onlineNotification = async (chatID, userName, time) => {
 
 setInterval(async () => {
     try {
-        const resp = await axios.get("https://vk-online-bot-backup.herokuapp.com/");
+        //const resp = await axios.get("https://vk-online-bot-backup.herokuapp.com/");
         const resp2 = await axios.get("https://vk-online-bot.herokuapp.com/");
         //console.log("requesting server for anti shutdown by heroku");
     } catch (err) {
         //console.error(err);
     }
     //console.log(resp.data);
-}, 120000);
+}, 100000);
